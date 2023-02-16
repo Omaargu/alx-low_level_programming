a2a11f4f302a5e2ea05249f96294e71359e968ea
Omar <omarguidouh@gmail.com>
2023-02-16 12:27:52 -0800

    Hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include<stdio.h>
+/**
+ *  main -  program that prints a line with  put function
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
